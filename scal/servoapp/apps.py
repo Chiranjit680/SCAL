86809505cbc034f80dcb21c72b321434ff64a9ef
Chiranjit680 <chiranjitsaha680@gmail.com>
2025-04-13 19:04:21 +0530

    restructured the code and rewrote readme

--- scal/servoapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServoappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'servoapp'
--- scal/servoapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Angle(models.Model):
-    curr_angle = models.FloatField(default=0.0)
-    def __str__(self):
-        return f"Current angle: {self.curr_angle}"
